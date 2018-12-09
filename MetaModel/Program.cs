6d24277bc8b52bb7190e781ecdf7481e6f21c161
klesogor <lesogor.kirill@gmail.com>
2018-12-09 21:46:37 +0200

    Add project files.

---
+++ MetaModel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetaModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
