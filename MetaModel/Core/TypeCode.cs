2be24bc6e0aec730a0748dd49485e01897658446
@oleksiitymchenko <>
2018-12-11 22:04:11 +0200

    Implemented converter utils

---
+++ MetaModel/Core/TypeCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetaModel.Core
+{
+    public enum TypeCode
+    {
+        Int = 0,
+        Float = 1,
+        Double = 2,
+        String = 3,
+        DateTime = 4
+    }
+}
---
+++ MetaModel/Core/UnsopportedTypeException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetaModel.Core
+{
+    public class UnsopportedTypeException: Exception
+    {
+        public override string Message => "This type is not implemented yet";
+    }
+}
